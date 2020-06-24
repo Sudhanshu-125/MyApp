e0c4bca2d825691be6b4ac72be6f6c157629749b
Sudhanshu-125 <sudhanshu8379kumar@gmail.com>
2020-06-24 11:56:00 +0530

    My First Commit in Application

---
+++ MyApp/src/main/java/com/ofss/App.java
@@ -0,0 +1,13 @@
+package com.ofss;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculator c = new Calculator();
+		System.out.println(c.add(1, 2));
+
+	}
+
+}
